a86b1e837ca6f6824b96da750fe1d8f575e57a5d
Pedro Rodrigues <pedroaugusto2610@poli.ufrj.br>
2023-09-29 20:01:16 -0300

    Correções iniciais.

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,89 @@
-import { useEffect, useState } from "react"
-import ITarefa from "./interfaces/ITarefa"
-import ListaTarefas from "./components/ListaTarefas"
+import { useEffect, useState } from "react";
+import ITarefa from "./interfaces/ITarefa";
+import ListaTarefas from "./components/ListaTarefas";
 
 function App() {
-  let textoNovaTarefa = '';
+  const [textoNovaTarefa, setTextoNovaTarefa] = useState("");
   const [tarefas, setTarefas] = useState<ITarefa[]>(() => {
     const tarefasSalvas = localStorage.getItem("tarefas");
-    return tarefasSalvas?.length ? JSON.parse(tarefasSalvas) : []
+    return tarefasSalvas ? JSON.parse(tarefasSalvas) : [];
   });
 
   useEffect(() => {
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
-  }, [tarefas])
-
-  let tarefasIncompletas: ITarefa[] = [];
-  let tarefasCompletas: ITarefa[] = [];
-
-  tarefas.forEach(tarefa => {
-    if (!tarefa.completa) {
-      tarefasIncompletas.push(tarefa);
-    }
-  });
-
-  tarefas.forEach(tarefa => {
-    if (tarefa.completa) {
-      tarefasCompletas.push(tarefa);
-    }
-  });
+  }, [tarefas]);
 
   const handleNovaTarefa = (textoNovaTarefa: string) => {
-    setTarefas(tarefas.concat({
-      id: Date.now(),
-      tarefa: textoNovaTarefa,
-      completa: false
-    }));
-
-    textoNovaTarefa = '';
-  }
+    if (textoNovaTarefa.trim() !== "") {
+      setTarefas([
+        ...tarefas,
+        {
+          id: Date.now(),
+          tarefa: textoNovaTarefa,
+          completa: false,
+        },
+      ]);
+      setTextoNovaTarefa(""); // Limpa o campo de entrada
+    }
+  };
 
   const handleTarefaCompleta = (tarefaAtualizar: ITarefa) => {
-    const tarefasAtualizadas = tarefas.map(tarefa => (
+    const tarefasAtualizadas = tarefas.map((tarefa) =>
       tarefa.id === tarefaAtualizar.id
         ? { ...tarefa, completa: !tarefa.completa }
         : tarefa
-    ))
+    );
 
     setTarefas(tarefasAtualizadas);
 
-    const quantidadeTarefasIncompletas = tarefas.filter(tarefa => !tarefa.completa).length;
-    document.title = `${quantidadeTarefasIncompletas} tarefas incompletas!`
-  }
+    const quantidadeTarefasIncompletas = tarefasAtualizadas.filter(
+      (tarefa) => !tarefa.completa
+    ).length;
+    document.title = `${quantidadeTarefasIncompletas} tarefas incompletas!`;
+  };
 
   const handleApagarTarefa = (tarefaApagar: ITarefa) => {
-    const tarefasAtualizadas = tarefas.filter(tarefa => tarefa.id !== tarefaApagar.id)
+    const tarefasAtualizadas = tarefas.filter(
+      (tarefa) => tarefa.id !== tarefaApagar.id
+    );
     setTarefas(tarefasAtualizadas);
-  }
+  };
 
   return (
     <div className="conteudo">
       <p>
-            <label htmlFor="nova-tarefa">Adicionar Tarefa</label>
-            <input
-                id="nova-tarefa"
-                type="text"
-                value={textoNovaTarefa}
-                onChange={e => textoNovaTarefa = e.target.value}
-            />
-            <button
-                id="botao-adicionar"
-                onClick={() => handleNovaTarefa(textoNovaTarefa)}
-            >Adicionar</button>
-        </p>
-        <ListaTarefas
-          titulo="Tarefas"
-          divId="incompletas"
-          tarefas={tarefasIncompletas}
-          handleTarefaCompleta={handleTarefaCompleta}
-          handleApagarTarefa={handleApagarTarefa}
-          linkTitulo="incompletas"
+        <label htmlFor="nova-tarefa">Adicionar Tarefa</label>
+        <input
+          id="nova-tarefa"
+          type="text"
+          value={textoNovaTarefa}
+          onChange={(e) => setTextoNovaTarefa(e.target.value)}
         />
-        <ListaTarefas
-          titulo="Completas"
-          divId="completas"
-          tarefas={tarefasCompletas}
-          handleTarefaCompleta={handleTarefaCompleta}
-          handleApagarTarefa={handleApagarTarefa}
-          linkTitulo="completas"
-        />
+        <button
+          id="botao-adicionar"
+          onClick={() => handleNovaTarefa(textoNovaTarefa)}
+        >
+          Adicionar
+        </button>
+      </p>
+      <ListaTarefas
+        titulo="Tarefas"
+        divId="incompletas"
+        tarefas={tarefas.filter((tarefa) => !tarefa.completa)}
+        handleTarefaCompleta={handleTarefaCompleta}
+        handleApagarTarefa={handleApagarTarefa}
+        linkTitulo="incompletas"
+      />
+      <ListaTarefas
+        titulo="Completas"
+        divId="completas"
+        tarefas={tarefas.filter((tarefa) => tarefa.completa)}
+        handleTarefaCompleta={handleTarefaCompleta}
+        handleApagarTarefa={handleApagarTarefa}
+        linkTitulo="completas"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
